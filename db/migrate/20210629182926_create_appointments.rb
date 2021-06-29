c04bdd56ffbaa9771a6a5730f792755ccb1b7849
Naomi-Rhames <naomirhames2020@gmail.com>
2021-06-29 13:58:23 -0500

    some message

---
+++ db/migrate/20210629182926_create_appointments.rb
@@ -0,0 +1,9 @@
+class CreateAppointments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :appointments do |t|
+      t.integer :doctors_id
+      t.integer :patients_id
+      t.timestamps
+    end
+  end
+end
