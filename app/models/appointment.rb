9edc7777afc8506b6fd1fcefdd1f9d3eaf736f71
Naomi-Rhames <naomirhames2020@gmail.com>
2021-07-26 08:34:43 -0500

    optional tasks

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,4 +7,5 @@ class Appointment < ApplicationRecord
     validates :veterinarian_id, presence: true
     validates :dog_id, presence: true
     
+    #OPTIMIZE: create scope method per logged in user
 end
