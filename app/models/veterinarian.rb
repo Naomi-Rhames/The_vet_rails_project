c1394055149b721e998dc284eda1de1003315cfa
Naomi-Rhames <naomirhames2020@gmail.com>
2021-07-26 18:30:34 -0500

    fix realationship

--- app/models/veterinarian.rb
+++ app/models/veterinarian.rb
@@ -1,9 +1,11 @@
 class Veterinarian < ApplicationRecord
-    has_many :appointment
-    has_many :dogs, through: :appointments
+    has_many :appointments, through: :dogs
+    has_many :dogs
     has_many :users, through: :dogs
 
     def full_name
         "#{self.name}".split(" ").map{|word| word.capitalize}.join(" ")
+
     end
 end
+    
