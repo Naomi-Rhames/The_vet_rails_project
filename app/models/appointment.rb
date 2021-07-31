782dedf8e9561783a1d8350de7e81df5c5670e63
Naomi-Rhames <naomirhames2020@gmail.com>
2021-07-30 23:50:04 -0500

    remove some validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,8 +4,9 @@ class Appointment < ApplicationRecord
     validates :symptoms, presence: true
     validates :agenda, presence: true
     validates :date, presence: true, uniqueness: true
-    validates :veterinarian_id, presence: true
     validates :dog_id, presence: true
     
     #OPTIMIZE: create scope method per logged in user  
+   
+
 end
